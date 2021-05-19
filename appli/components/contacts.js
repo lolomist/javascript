0d21837adca4e8b03faf83c81ffd2a732c67a1fb
lolomist <55925903+lolomist@users.noreply.github.com>
2021-05-19 02:31:16 +0200

    clearInterval
      componentWillUnmount() {
        clearInterval(this.intervalID);
      }

--- appli/components/contacts.js
+++ appli/components/contacts.js
@@ -27,6 +27,10 @@ class App extends Component {
     this.sendFriendInvite = this.sendFriendInvite.bind(this);
   }
 
+  componentWillUnmount() {
+    clearInterval(this.intervalID);
+  }
+
   componentDidMount() {
     console.log("contact");
     GLOBALS.SOCKET.on('getContacts', data => {
